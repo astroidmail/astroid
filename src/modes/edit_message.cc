1e281dc78667a578aef60d3f1718f99a5e5b175e
Gaute Hope <eg@gaute.vetsj.com>
2014-04-26 19:38:53 +0200

    em: navigate fields

--- src/modes/edit_message.cc
+++ src/modes/edit_message.cc
@@ -1,4 +1,6 @@
 # include <iostream>
+# include <vector>
+# include <algorithm>
 
 # include "astroid.hh"
 # include "edit_message.hh"
@@ -13,11 +15,99 @@ namespace Astroid {
     Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("ui/edit-message.glade", "box_message");
 
     builder->get_widget ("box_message", box_message);
+
+    builder->get_widget ("from", from);
+    builder->get_widget ("to", to);
+    builder->get_widget ("cc", cc);
+    builder->get_widget ("bcc", bcc);
+
+    /* must be in the same order as enum Field */
+    fields.push_back (from);
+    fields.push_back (to);
+    fields.push_back (cc);
+    fields.push_back (bcc);
+
     pack_start (*box_message, true, 5);
 
     show_all ();
 
+    activate_field (From);
   }
+
+  void EditMessage::activate_field (Field f) {
+    cout << "em: activate field: " << f << endl;
+    reset_fields ();
+
+    current_field = f;
+
+    if (in_edit) {
+      fields[current_field]->set_icon_from_icon_name ("go-next");
+      fields[current_field]->set_sensitive (true);
+    } else {
+      fields[current_field]->set_icon_from_icon_name ("media-playback-stop");
+    }
+    fields[current_field]->grab_focus ();
+  }
+
+  void EditMessage::reset_fields () {
+    for_each (fields.begin (),
+              fields.end (),
+              [&](Gtk::Entry * e) {
+                reset_entry (e);
+              });
+  }
+
+  void EditMessage::reset_entry (Gtk::Entry *e) {
+    e->set_icon_from_icon_name ("");
+    e->set_activates_default (true);
+    fields[current_field]->set_sensitive (false);
+  }
+
+  bool EditMessage::on_key_press_event (GdkEventKey * event) {
+    cout << "em: got key press" << endl;
+    switch (event->keyval) {
+      case GDK_KEY_j:
+        if (in_edit) return false; // otherwise act as Tab
+      case GDK_KEY_Tab:
+        {
+          activate_field ((Field) ((current_field+1) % ((int)fields.size())));
+          return true;
+        }
+
+      case GDK_KEY_k:
+        if (in_edit) {
+          return false;
+        } else {
+          activate_field ((Field) (((current_field)+((int)fields.size())-1) % ((int)fields.size())));
+          return true;
+        }
+
+      case GDK_KEY_Return:
+        {
+          if (!in_edit) {
+            in_edit = true;
+            activate_field (current_field);
+          } else {
+            // go to next field
+            activate_field ((Field) ((current_field+1) % ((int)fields.size())));
+          }
+          return true;
+        }
+
+      case GDK_KEY_Escape:
+        {
+          if (in_edit) {
+            in_edit = false;
+            activate_field (current_field);
+          }
+          return true;
+        }
+
+      default:
+        return false; // if from field, field will handle event
+    }
+  }
+
 
   void EditMessage::grab_modal () {
     add_modal_grab ();
--- src/modes/edit_message.hh
+++ src/modes/edit_message.hh
@@ -1,5 +1,7 @@
 # pragma once
 
+# include <vector>
+
 # include "astroid.hh"
 # include "mode.hh"
 
@@ -12,9 +14,32 @@ namespace Astroid {
 
       Gtk::Box * box_message;
 
+      enum Field {
+        From = 0,
+        To,
+        Cc,
+        Bcc,
+        Message,
+      };
+
+      Gtk::Entry * from, *to, *cc, *bcc;
+      vector<Gtk::Entry *> fields;
+
+      Field current_field = From;
+      void activate_field (Field);
+
+    private:
+      void reset_fields ();
+      void reset_entry (Gtk::Entry *);
+
+      bool in_edit = false;
+
+    public:
       void grab_modal () override;
       void release_modal () override;
 
+    protected:
+      virtual bool on_key_press_event (GdkEventKey *) override;
   };
 }
 
