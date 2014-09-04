325a2e4abcecb849d87cde49f768f7bbb849b870
Gaute Hope <eg@gaute.vetsj.com>
2014-09-04 09:12:11 +0200

    mq: slow..

--- src/utils/mail_quote.cc
+++ src/utils/mail_quote.cc
@@ -13,8 +13,6 @@ namespace Astroid {
     /* run through html body and wrap quoted parts of the body with
      * quote divs */
 
-    log << debug << "mq: filter quotes: " << body << endl;
-
     ustring quote = "&gt;";
     ustring newline = "<br>";
 
@@ -26,14 +24,10 @@ namespace Astroid {
     bool in_quote_prefix = false;
     int last_quote_start = 0;
 
-    bool drop_quote = true;
-
     ustring start_quote = "<blockquote>";
     ustring stop_quote  = "</blockquote>";
 
     for (int i = 0; i < static_cast<int>(body.length()); i++) {
-      log << debug << "mq: i = " << i << endl;
-
       if (body[i] == ' ') continue; // skip spaces
       if (body[i] == '\n') continue; // skip newlines
 
@@ -41,42 +35,35 @@ namespace Astroid {
 
       /* look for newlines */
       if (body.compare (i, 4, newline) == 0) {
-        log << debug << "mq: found newline." << endl;
-        i += 3;
+        i += 4;
         if (in_newline) {
           /* double newline means end of all quotes */
-          i++;
 
-          log << debug << "mq: double new line, closing quotes: " << current_quote_level << endl;
           for (; current_quote_level > 0; current_quote_level--) {
-            log << debug << "mq: closing blockquote." << endl;
             body.insert (i, stop_quote);
             i += stop_quote.length();
           }
 
-          new_line_now = true;
 
         } else {
           in_newline = true;
-          new_line_now = true;
         }
+
+        new_line_now = true;
       }
 
       if (body.compare (i, 4, quote) == 0) {
-        log << debug << "mq: found quote, level: " << quote_level << endl;
-
         if (in_quote_prefix) {
           quote_level++;
         } else {
           if (in_newline) {
-            log << debug << "mq: first after new line, starting counting." << endl;
             /* only start if are after a new line */
             in_quote_prefix = true;
             quote_level = 1;
             last_quote_start = i;
             in_newline = false;
           } else {
-            log << debug << "mq: not in new line, skipping." << endl;
+            /* quote not after newline, skipping */
           }
         }
 
@@ -94,7 +81,6 @@ namespace Astroid {
 
           } else if (quote_level < current_quote_level) {
             current_quote_level--;
-            if (new_line_now) i++;
             body.insert (i, stop_quote);
 
           }
@@ -111,9 +97,8 @@ namespace Astroid {
       }
     }
 
-    log << debug << "mq: closing all blockquotes." << endl;
+    /* close up all remaining quotes */
     for (; current_quote_level > 0; current_quote_level--) {
-      log << debug << "mq: closing blockquote." << endl;
       body.insert (body.length()-1, stop_quote);
     }
   }
