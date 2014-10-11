d84c7a65697d3058c92d06b6441a18146fcb2d02
Gaute Hope <eg@gaute.vetsj.com>
2014-10-11 18:22:50 +0200

    tests: generic setup test

---
+++ test/test_common.hh
@@ -0,0 +1,8 @@
+# include "astroid.hh"
+using namespace Astroid;
+
+void setup () {
+  Astroid::astroid = new Astroid::Astroid ();
+  astroid->main_test ();
+}
+
--- test/test_generic.cc
+++ test/test_generic.cc
@@ -1,19 +1,16 @@
 # define BOOST_TEST_DYN_LINK
-# define BOOST_TEST_MODULE TestCompose 
+# define BOOST_TEST_MODULE TestGeneric
 # include <boost/test/unit_test.hpp>
 
 # include "test_common.hh"
 # include "compose_message.hh"
 using namespace std;
 
-BOOST_AUTO_TEST_SUITE(Composing)
+BOOST_AUTO_TEST_SUITE(Generic)
 
-  BOOST_AUTO_TEST_CASE(compose_read_test)
+  BOOST_AUTO_TEST_CASE(setup_test)
   {
     setup ();
-
-    ComposeMessage * c = new ComposeMessage ();
-
   }
 
 BOOST_AUTO_TEST_SUITE_END()
