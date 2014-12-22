ab4ee40c1b235313217054304ae58d183a31a35b
Gaute Hope <eg@gaute.vetsj.com>
2014-12-22 11:36:31 +0100

    #14: include script for making compile_commands using bear

---
+++ devel/make_compile_commands.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+#
+# this uses bear to make compile_commands.json for for SCons
+#
+# https://github.com/gauteh/astroid/issues/14
+#
+# run from root source directory.
+
+scons -c
+bear -- scons --verbose
+
