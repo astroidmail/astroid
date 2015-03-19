991fa2070d6b7aa3ea4c1375d8a726b869c2419d
Gaute Hope <eg@gaute.vetsj.com>
2015-03-19 09:16:11 +0100

    ycm: directoryofthisscript

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -146,8 +146,8 @@ def DirectoryOfThisScript():
 #
 # Most projects will NOT need to set this to anything; you can just change the
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
-#compilation_database_folder = DirectoryOfThisScript ()
-compilation_database_folder = ''
+compilation_database_folder = DirectoryOfThisScript ()
+#compilation_database_folder = ''
 
 if os.path.exists( compilation_database_folder ):
   database = ycm_core.CompilationDatabase( compilation_database_folder )
