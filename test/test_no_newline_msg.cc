ef4f6924b698f2f867420e16c2861ca2d4a64087
Gaute Hope <eg@gaute.vetsj.com>
2015-05-28 15:46:59 +0200

    test: no newline after only-link email is not shown

--- test/test_no_newline_msg.cc
+++ test/test_no_newline_msg.cc
@@ -29,5 +29,27 @@ BOOST_AUTO_TEST_SUITE(Reading)
     teardown ();
   }
 
+  BOOST_AUTO_TEST_CASE(reading_no_new_line_after_link)
+  {
+    /* this fails if there is a link at the end of a part (plain-text at
+     * least) that does not have any other text before it */
+
+    setup ();
+
+    ustring fname = "test/mail/test_mail/no-nl-link.eml";
+
+    Message m (fname);
+
+    ustring text =  m.viewable_text(false);
+    BOOST_CHECK (text.find ("line-ignored.com") != ustring::npos);
+
+    ustring html = m.viewable_text(true);
+    cout << "html: " << html << endl;
+    BOOST_CHECK (html.find ("line-ignored.com") != ustring::npos);
+
+
+    teardown ();
+  }
+
 BOOST_AUTO_TEST_SUITE_END()
 
