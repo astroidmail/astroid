4fbe42640e96df3d53a3b950a97aa1d3901eb105
Gaute Hope <eg@gaute.vetsj.com>
2016-01-15 10:29:33 +0100

    theme: test loading and processing scss

---
+++ test/test_theme.cc
@@ -0,0 +1,33 @@
+# define BOOST_TEST_DYN_LINK
+# define BOOST_TEST_MODULE TestTheme
+# include <boost/test/unit_test.hpp>
+
+# include "test_common.hh"
+# include "glibmm.h"
+
+# include "modes/thread_view/theme.hh"
+
+
+using namespace std;
+
+
+BOOST_AUTO_TEST_SUITE(Theme)
+
+  BOOST_AUTO_TEST_CASE(loading_theme)
+  {
+    setup ();
+
+    Astroid::Theme * t;
+    BOOST_CHECK_NO_THROW (t = new Astroid::Theme ());
+
+    ustring css = t->thread_view_css;
+    ustring html = t->thread_view_html;
+
+    delete t;
+
+    teardown ();
+  }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
