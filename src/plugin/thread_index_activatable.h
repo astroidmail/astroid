cc0e73c0c544ff7c04cbc7aa064dac9d6d96a492
Gaute Hope <eg@gaute.vetsj.com>
2016-06-12 23:14:45 +0200

    wip: threadindex activatable

---
+++ src/plugin/thread_index_activatable.h
@@ -0,0 +1,39 @@
+# pragma once
+
+# include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define ASTROID_THREADINDEX_TYPE_ACTIVATABLE		(astroid_threadindex_activatable_get_type ())
+
+#define ASTROID_THREADINDEX_ACTIVATABLE(obj)		(G_TYPE_CHECK_INSTANCE_CAST ((obj), ASTROID_THREADINDEX_TYPE_ACTIVATABLE, AstroidThreadIndexActivatable))
+
+#define ASTROID_THREADINDEX_ACTIVATABLE_IFACE(obj)	(G_TYPE_CHECK_CLASS_CAST ((obj), ASTROID_THREADINDEX_TYPE_ACTIVATABLE, AstroidThreadIndexActivatableInterface))
+
+#define ASTROID_IS_THREADINDEX_ACTIVATABLE(obj)	(G_TYPE_CHECK_INSTANCE_TYPE ((obj), ASTROID_THREADINDEX_TYPE_ACTIVATABLE))
+#define ASTROID_THREADINDEX_ACTIVATABLE_GET_IFACE(obj)	(G_TYPE_INSTANCE_GET_INTERFACE ((obj), ASTROID_THREADINDEX_TYPE_ACTIVATABLE, AstroidThreadIndexActivatableInterface))
+
+typedef struct _AstroidThreadIndexActivatable AstroidThreadIndexActivatable;
+typedef struct _AstroidThreadIndexActivatableInterface AstroidThreadIndexActivatableInterface;
+
+struct _AstroidThreadIndexActivatableInterface
+{
+	GTypeInterface g_iface;
+
+	void (*activate) (AstroidThreadIndexActivatable * activatable);
+	void (*deactivate) (AstroidThreadIndexActivatable * activatable);
+	void (*update_state) (AstroidThreadIndexActivatable * activatable);
+
+};
+
+GType astroid_threadindex_activatable_get_type (void) G_GNUC_CONST;
+
+void astroid_threadindex_activatable_activate (AstroidThreadIndexActivatable *activatable);
+
+void astroid_threadindex_activatable_deactivate (AstroidThreadIndexActivatable *activatable);
+
+void astroid_threadindex_activatable_update_state (AstroidThreadIndexActivatable *activatable);
+
+
+G_END_DECLS
+
