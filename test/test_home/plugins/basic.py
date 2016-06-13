c523be01f1b58365ed7e6d97c3801daf7c6a5ab5
Gaute Hope <eg@gaute.vetsj.com>
2016-06-13 11:12:43 +0200

    test: add plugins to test

---
+++ test/test_home/plugins/basic.py
@@ -0,0 +1,28 @@
+import gi
+gi.require_version ('Astroid', '0.1')
+from gi.repository import GObject, Astroid
+
+print ("basic: plugin loading..")
+
+class BasicPlugin (GObject.Object, Astroid.Activatable):
+  object = GObject.property (type=GObject.Object)
+
+  def do_activate (self):
+    print ('basic: activate')
+
+  def do_deactivate (self):
+    print ('basic: deactivate')
+
+  def do_ask (self, n):
+    print (repr(n))
+    print ("got ask:", n)
+
+    from IPython import embed
+    # embed ()
+
+
+    n = n.replace ("hello", "asdf")
+    print (n)
+
+    return n
+
