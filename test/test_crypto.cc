08af1006e8e861b1c5057c89263a6d965093a946
Gaute Hope <eg@gaute.vetsj.com>
2017-02-21 09:11:14 +0100

    crypto: test: provoke 'Too many open files' error'

--- test/test_crypto.cc
+++ test/test_crypto.cc
@@ -7,9 +7,10 @@
 # include "crypto.hh"
 # include "message_thread.hh"
 # include "account_manager.hh"
+# include "db.hh"
 
 
-BOOST_AUTO_TEST_SUITE(PGPEncryption)
+BOOST_AUTO_TEST_SUITE(GPGEncryption)
 
   BOOST_AUTO_TEST_CASE(crypto_compose_simple_test)
   {
@@ -43,7 +44,7 @@ BOOST_AUTO_TEST_SUITE(PGPEncryption)
     c->set_from (&a);
     c->set_to ("astrid@astroidmail.bar");
     c->encrypt =  true;
-    c->sign = false; // passphrase doesn't work with gpg2
+    c->sign = false;
 
     ustring bdy = "This is test: æøå.\n > testing\ntesting\n...";
 
@@ -65,8 +66,6 @@ BOOST_AUTO_TEST_SUITE(PGPEncryption)
     LOG (test) << "cm: deleting ComposeMessage..";
 
     delete c;
-
-    /* need passphrase, doesn't work yet */
 
     Message m (fn);
     ustring rbdy = m.viewable_text (false);
@@ -160,6 +159,119 @@ BOOST_AUTO_TEST_SUITE(PGPEncryption)
 
     delete c;
 
+
+    teardown ();
+  }
+
+  BOOST_AUTO_TEST_CASE (crypto_too_many_open_files)
+  {
+    using Astroid::ComposeMessage;
+    using Astroid::Account;
+    using Astroid::Message;
+    using Astroid::MessageThread;
+    using Astroid::message_error;
+    using Astroid::Db;
+    using Astroid::NotmuchThread;
+    setup ();
+
+    Account a = astroid->accounts->accounts[0];
+    LOG (trace) << "cy: account gpg: " << a.gpgkey;
+    a.email = "gaute@astroidmail.bar";
+
+    /* create a test message */
+    ComposeMessage * c = new ComposeMessage ();
+    c->set_from (&a);
+    c->set_to ("astrid@astroidmail.bar");
+    c->encrypt =  true;
+    c->sign = true;
+
+    ustring bdy = "This is test: æøå.\n > testing\ntesting\n...";
+
+    c->body << bdy;
+    c->set_subject ("test gpg e-mail");
+    ustring mid = "test-gpg-1@astroid";
+    c->set_id (mid);
+
+    c->build ();
+    c->finalize ();
+
+    ustring fn = "test/mail/test_mail/gpg-test-1.eml";
+    c->write (fn);
+    system ("notmuch new");
+
+    BOOST_CHECK_MESSAGE (c->encryption_success == true, "encryption should be successful");
+    LOG (test) << "cm: deleting ComposeMessage..";
+
+    delete c;
+
+    Message *m;
+
+    Db db(Db::DATABASE_READ_ONLY);
+    db.on_message (mid, [&](notmuch_message_t * msg) {
+        LOG (test) << "trying to open encrypted file.";
+
+        m = new Message (msg, 0);
+
+        });
+
+    /* check that body matches */
+    ustring rbdy = m->viewable_text (false);
+    BOOST_CHECK_MESSAGE (bdy == rbdy, "message reading produces the same output as compose message input");
+
+    /* notmuch thread id */
+    ustring tid = m->tid;
+    delete m;
+
+    refptr<NotmuchThread> nt;
+
+    db.on_thread (tid, [&](notmuch_thread_t * nmt) {
+
+        nt = refptr<NotmuchThread> (new NotmuchThread (nmt));
+
+        });
+    db.close ();
+
+    /* Okay, let's see if we can provoke 'Too many open files' in GPG */
+    int tries = 0;
+    bool failed = false;
+
+    try {
+      for (int i = 0; i < 100; i ++) {
+        LOG (info) << "loading message thread..";
+        refptr<MessageThread> mthread = refptr<MessageThread>(new MessageThread (nt));
+        Db db (Db::DATABASE_READ_ONLY);
+        mthread->load_messages (&db);
+        tries++;
+      }
+    } catch (Astroid::message_error &ex) {
+      LOG (error) << "caught too many files error: " << ex.what ();
+      failed = true;
+    }
+
+    /* let's try without using refptr */
+    /*
+    LOG (info) << "loading without using refptr..";
+    try {
+      for (int i = 0; i < 1200; i ++) {
+        LOG (info) << "loading message thread..";
+        MessageThread mthread (nt);
+        Db db (Db::DATABASE_READ_ONLY);
+        mthread.load_messages (&db);
+        tries++;
+      }
+    } catch (Astroid::message_error &ex) {
+      LOG (error) << "caught too many files error: " << ex.what ();
+      failed = true;
+    }
+    */
+
+    LOG (warn) << "opened a GPG e-mail: " << tries << " times.";
+
+    BOOST_CHECK_MESSAGE (failed == false, "Caught 'Too many open files error'");
+
+    /* removing test encrypted file */
+    unlink (fn.c_str ());
+    system ("notmuch new");
 
     teardown ();
   }
