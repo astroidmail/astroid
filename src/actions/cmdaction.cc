26ab5ed192441dc2b7fceb0fcf9a242245deb9f7
Hugo Roy <hugo@hugoroy.eu>
2017-03-02 21:54:45 +0100

    implement run hooks on thread view (see #29)
    the `thread_view.run` hook takes two arguments:
    
    %1: thread_id
    %2: message_id of currently focused message

--- src/actions/cmdaction.cc
+++ src/actions/cmdaction.cc
@@ -24,11 +24,21 @@ namespace Astroid {
   }
 
   void CmdAction::emit (Db * db) {
+    /* if there is a thread_id given: we cannot know if only the thread or only
+     * the messages has been modified in the hook. we therefore always have to
+     * emit a full 'thread-update' signal. this _should_ cause all messages in
+     * the thread, including the one passed as mid, to be updated. emitting a
+     * second 'messsage-updated' is therefore redundant in these cases.
+     */
+
     if (thread_id != "") {
+      // will also cause all messages in thread to be updated
       astroid->actions->emit_thread_updated (db, thread_id);
+      return;
     }
 
     if (mid != "") {
+      // will also emit thread_changed
       astroid->actions->emit_message_updated (db, mid);
     }
   }
