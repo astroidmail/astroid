e6667ad4eaf124becc349d473b2c5310284a7c34
Gaute Hope <eg@gaute.vetsj.com>
2017-03-27 14:01:00 +0200

    mt: show missing content warning when failing to create GMimeStream

--- src/message_thread.cc
+++ src/message_thread.cc
@@ -152,8 +152,8 @@ namespace Astroid {
       missing_content = true;
 
       if (in_notmuch) {
-        LOG (warn) << "loading cache for missing file from notmuch";
 
+        LOG (warn) << "loading cache for missing file from notmuch";
         load_notmuch_cache ();
 
       } else {
@@ -162,19 +162,29 @@ namespace Astroid {
         string error_s = "failed to open file: " + fname;
         throw message_error (error_s.c_str());
       }
+      return;
 
     } else {
       GMimeStream   * stream  = g_mime_stream_file_new_for_path (fname.c_str(), "r");
       if (stream == NULL) {
         LOG (error) << "failed to open file: " << fname << " (unspecified error)";
-        string error_s = "failed to open file: " + fname;
-        throw message_error (error_s.c_str());
+
+        has_file = false;
+        missing_content = true;
+
+        if (in_notmuch) {
+          LOG (warn) << "loading cache for missing file from notmuch";
+          load_notmuch_cache ();
+        } else {
+          LOG (error) << "tried to open disk file, but failed, message is not in database either.";
+          string error_s = "failed to open file: " + fname;
+          throw message_error (error_s.c_str());
+        }
+        return;
       }
 
       GMimeParser   * parser  = g_mime_parser_new_with_stream (stream);
-
       GMimeMessage * _message = g_mime_parser_construct_message (parser);
-
       load_message (_message);
 
       g_object_unref (_message); // is reffed in load_message
