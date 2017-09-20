67a6633ae2db26660fad47c59a3f26fca790712c
Gaute Hope <eg@gaute.vetsj.com>
2017-09-20 09:25:19 +0200

    webkit2: initial javascript

---
+++ ui/thread-view.js
@@ -0,0 +1,9 @@
+/* Incomplete list of functionality that needs to be implemented:
+ *
+ * Messages:
+ *  update_tags (change rendered list of tags)
+ *  update_css_tags (change corresponding CSS tags) can go together with the one above
+ *
+ *
+ *
+ */
