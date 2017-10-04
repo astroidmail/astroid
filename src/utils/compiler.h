dfd9e2a46667b46958a6a35d12c94dcbebfb57a4
Alexander Adolf <alexander.adolf@condition-alpha.com>
2017-10-04 15:09:01 +0200

    compiler: should use __has_cpp_attribute and check for availability
    The build failed when compiling src/utils/gmime/gmime-filter-html-bq.c.
    It complains about a missing ')' after 'clang' in line 5 of compiler.h.
    This is because clang::fallthrough is a scoped attribute, but the used
    macro __has_attribute will only accept single identifiers under clang.
    Hence, its sibling __has_cpp_attribute must be used. Then, when using
    the LLVM clang that ships with Xcode, the __has_cpp_attribute is not
    defined at all when compiling gmime-filter-html-bq.c. This is because it
    is a C file, and the LLVM clang that ships with Xcode hides the C++
    features (including the feature checking macros) when operating in C
    mode. Thus we must safeguard against the __has_* macros not being
    defined. For further details, see
    
    https://isocpp.org/std/standing-documents/sd-6-sg10-feature-test-recommendations
    
    and
    
    https://clang.llvm.org/docs/LanguageExtensions.html#feature-checking-macros
    
    Signed-off-by: Gaute Hope <eg@gaute.vetsj.com>

--- src/utils/compiler.h
+++ src/utils/compiler.h
@@ -2,10 +2,28 @@
 
 # if defined (__clang__)
 
-# if __has_attribute(clang::fallthrough)
-# define FALLTHROUGH [[clang::fallthrough]]
+# ifndef __has_attribute
+#   define __has_attribute(x) 0
+# endif
+# ifndef __has_cpp_attribute
+#   define __has_cpp_attribute(x) 0
+# endif
+# ifndef __has_builtin
+#   define __has_builtin(x) 0
+# endif
+# ifndef __has_feature
+#   define __has_feature(x) 0
+# endif
+# ifndef __has_extension
+#   define __has_extension(x) 0
+# endif
+# ifndef __has_warning
+#   define __has_warning(x) 0
+# endif
+# if __has_cpp_attribute(clang::fallthrough)
+#   define FALLTHROUGH [[clang::fallthrough]]
 # else
-# define FALLTHROUGH
+#   define FALLTHROUGH
 # endif
 
 # elif defined (__GNUC__)
