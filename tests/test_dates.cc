29b15e8e1d4fc4cee893da9b7596cc1caf332e8a
Gaute Hope <eg@gaute.vetsj.com>
2017-10-10 23:35:09 +0200

    tests: test timezone on gmime2 and gmime3

--- tests/test_dates.cc
+++ tests/test_dates.cc
@@ -2,12 +2,15 @@
 # define BOOST_TEST_MODULE TestDates
 # include <boost/test/unit_test.hpp>
 # include <gmime/gmime.h>
+# include "utils/gmime/gmime-compat.h"
 
 # include "test_common.hh"
 # include "message_thread.hh"
+# include "compose_message.hh"
 # include "utils/date_utils.hh"
 
 using Astroid::Message;
+using Astroid::ComposeMessage;
 
 BOOST_AUTO_TEST_SUITE(Dates)
 
@@ -40,27 +43,28 @@ BOOST_AUTO_TEST_SUITE(Dates)
   BOOST_AUTO_TEST_CASE(dates_test_dates)
   {
     setup ();
+    /* setlocale (LC_ALL, "C"); */
 
-    std::vector<ustring> dates =
+    std::vector<std::pair<ustring,ustring>> dates =
     {
-      "Tue, 19 Aug 2014 16:11:44 +0200",
-      "Mon, 9 May 2016 13:26:57 +0000",
-      "Thu, 1 Sep 2016 14:32:16 +0000"
+      std::make_pair ("Tue, 19 Aug 2014 16:11:44 +0200", "Tue, 19 Aug 2014 16:11:44 +0200"),
+      std::make_pair ("Mon, 9 May 2016 13:26:57 +0000",  "Mon, 09 May 2016 13:26:57 +0000"),
+      std::make_pair ("Thu, 1 Sep 2016 14:32:16 +0000",  "Thu, 01 Sep 2016 14:32:16 +0000"),
     };
 
 
     Message a ("tests/mail/test_mail/date1.eml");
 
-    for (ustring d : dates) {
-      LOG (warn) << "date: testing: " << d;
+    for (auto d : dates) {
+      LOG (warn) << "date: testing: " << d.first;
 
       GMimeMessage * m = a.message;
       g_object_ref (m);
 
 # if (GMIME_MAJOR_VERSION < 3)
-      g_mime_message_set_date_as_string (m, d.c_str ());
+      g_mime_message_set_date_as_string (m, d.first.c_str ());
 # else
-      GDateTime * dt = g_mime_utils_header_decode_date (d.c_str ());
+      GDateTime * dt = g_mime_utils_header_decode_date (d.first.c_str ());
       g_mime_message_set_date (m, dt);
 # endif
 
@@ -72,9 +76,32 @@ BOOST_AUTO_TEST_SUITE(Dates)
       LOG (test) << "pretty_verbose_date: " << b.pretty_verbose_date ();
       LOG (test) << "pretty_verbose_date (true): " << b.pretty_verbose_date (true);
 
-
+      BOOST_CHECK (b.date () == d.second);
     }
 
+
+    LOG (test) << "Testing now date..";
+    /* test compose message date setting */
+    GMimeMessage * m = a.message;
+    g_object_ref (m);
+
+    /* hopefully these happen within the same second.. */
+    GDateTime * now = g_date_time_new_now_local ();
+    g_mime_message_set_date_now (m);
+
+    Message b (m);
+    LOG (test) << "date: " << b.date ();
+    LOG (test) << "date, asctime: " << b.date_asctime ();
+    LOG (test) << "pretty_date: " << b.pretty_date ();
+    LOG (test) << "pretty_verbose_date: " << b.pretty_verbose_date ();
+    LOG (test) << "pretty_verbose_date (true): " << b.pretty_verbose_date (true);
+
+    const char * datefmt = "%a, %d %b %Y %T %z";
+    setlocale (LC_ALL, "C");
+    gchar * dt = g_date_time_format (now, datefmt);
+    setlocale (LC_ALL, "");
+    LOG (test) << "formatted date: " << dt;
+    BOOST_CHECK (dt == b.date ());
 
     teardown ();
   }
