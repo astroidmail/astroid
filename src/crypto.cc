2fa87d3af711cea44b05952defb91c95140c6ce7
Gaute Hope <eg@gaute.vetsj.com>
2017-11-03 12:32:37 +0100

    crypto: fix #427: only enable throw_keyids for gmime 3

--- src/crypto.cc
+++ src/crypto.cc
@@ -238,7 +238,11 @@ namespace Astroid {
         mo,
         sign,
         userid.c_str (),
-        GMIME_ENCRYPT_THROW_KEYIDS, // ignored in Gmime 2.6
+# if (GMIME_MAJOR_VERSION >= 3)
+        GMIME_ENCRYPT_THROW_KEYIDS, // only in Gmime 3
+# else
+        GMIME_ENCRYPT_NONE,
+# endif
         recpa,
         err);
 
