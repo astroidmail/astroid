astroidmail/astroid

commit 11f60370eb74cbf96c8f7dfdddef6be766414711

fix #491: opening next unread from thread-view causes crash when query is empty.

src/modes/thread_index/thread_index.cc
@@ -257,6 +257,11 @@ namespace Astroid {
   }
 
   bool ThreadIndex::on_index_action (ThreadView * tv, ThreadView::IndexAction action) {
+
+    if (list_view->filtered_store->children().size() < 2) {
+      return true;
+    }
+
     switch (action) {
       case ThreadView::IndexAction::IA_Next:
         {

commit cb5033ebdc150aac272ba2bc9f007ebd6a5781c5

fix off-by-one introduced in 11f60370: jumping to next thread from thread-view

src/modes/thread_index/thread_index.cc
@@ -258,7 +258,7 @@ namespace Astroid {
 
   bool ThreadIndex::on_index_action (ThreadView * tv, ThreadView::IndexAction action) {
 
-    if (list_view->filtered_store->children().size() < 2) {
+    if (list_view->filtered_store->children().size() < 1) {
       return true;
     }
 
@@ -267,8 +267,10 @@ namespace Astroid {
         {
           keys.handle ("thread_index.next_thread");
           auto thread = list_view->get_current_thread ();
-          tv->load_thread (thread);
-          tv->show ();
+          if (thread != tv->thread) {
+            tv->load_thread (thread);
+            tv->show ();
+          }
         }
         break;
 
@@ -276,8 +278,10 @@ namespace Astroid {
         {
           keys.handle ("thread_index.previous_thread");
           auto thread = list_view->get_current_thread ();
-          tv->load_thread (thread);
-          tv->show ();
+          if (thread != tv->thread) {
+            tv->load_thread (thread);
+            tv->show ();
+          }
         }
         break;
 
@@ -285,8 +289,10 @@ namespace Astroid {
         {
           keys.handle ("thread_index.next_unread");
           auto thread = list_view->get_current_thread ();
-          tv->load_thread (thread);
-          tv->show ();
+          if (thread != tv->thread) {
+            tv->load_thread (thread);
+            tv->show ();
+          }
         }
         break;
 
@@ -294,8 +300,10 @@ namespace Astroid {
         {
           keys.handle ("thread_index.previous_unread");
           auto thread = list_view->get_current_thread ();
-          tv->load_thread (thread);
-          tv->show ();
+          if (thread != tv->thread) {
+            tv->load_thread (thread);
+            tv->show ();
+          }
         }
         break;
     }