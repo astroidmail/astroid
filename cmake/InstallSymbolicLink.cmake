fef4d4537b1545ff49a6d699c6e2275d43c8ef46
Alexander Adolf <alexander.adolf@condition-alpha.com>
2018-07-05 16:46:03 +0200

    take DESTDIR environment variable into account (if and when set)

--- cmake/InstallSymbolicLink.cmake
+++ cmake/InstallSymbolicLink.cmake
@@ -1,6 +1,13 @@
+# shamelessly inspired by https://github.com/bro/cmake/blob/master/InstallSymlink.cmake
 macro(install_symlink filepath sympath)
   get_filename_component(symdir ${sympath} DIRECTORY)
   install(DIRECTORY DESTINATION ${symdir})
-  install(CODE "execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ${filepath} ${sympath})")
+  install(CODE "
+     if (\"\$ENV{DESTDIR}\" STREQUAL \"\")
+        execute_process(COMMAND \"${CMAKE_COMMAND}\" -E create_symlink ${filepath} ${sympath})
+     else ()
+        execute_process(COMMAND \"${CMAKE_COMMAND}\" -E create_symlink ${filepath} \$ENV{DESTDIR}/${sympath})
+     endif ()
+   ")
   install(CODE "message(\"-- Symlinking: ${sympath} -> ${filepath}\")")
 endmacro(install_symlink)
